3f656ac617333099a8c56dc1e4b5b6d3e6e4a7cd
sonderben <sonderben@gmail.com>
2023-07-04 01:09:45 -0400

    change background color

---
+++ logic/script.js
@@ -0,0 +1,22 @@
+
+// const toggle = document.querySelector("#toggle1");
+// toggle.addEventListener('click', function() {
+//   console.log('Button clicked!');
+// });
+
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const actionElement = document.querySelector('.action');
+    
+
+
+    const toggle = document.querySelector("#toggle1");
+    toggle.addEventListener('click', function() {
+        
+        
+    });
+
+
+})
